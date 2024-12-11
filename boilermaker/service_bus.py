2c6181c37de276d1a5222644c86481d66fbf8864
erewok <eraker@gmail.com>
2024-12-11 10:15:43 -0800

    Use new dependency and get rid of servicebus code

--- boilermaker/service_bus.py
+++ boilermaker/service_bus.py
@@ -4,69 +4,21 @@
     Wrapper class around a `ServiceBusClient` which allows sending messages or
     subscribing to a queue.
 """
-import datetime
 import logging
-
-from azure.servicebus import ServiceBusMessage
-from azure.servicebus.aio import ServiceBusClient, ServiceBusReceiver, ServiceBusSender
+from aio_azure_clients_toolbox import ManagedAzureServiceBusSender
 
 from .config import Config
+from .task import STATIC_DEBUG_TASK
 
 servicebus_logger = logging.getLogger("azure.servicebus")
 servicebus_logger.setLevel(logging.WARNING)
 
 
 class AzureServiceBus:
-    def __init__(self, settings: Config):
-        self.namespace_url = settings.service_bus_namespace_url
-        self.queue_name = settings.service_bus_queue_name
-        self.credential = settings.az_credential()
-        self._client: ServiceBusClient | None = None
-        self._receiver_client: ServiceBusReceiver | None = None
-        self._sender_client: ServiceBusSender | None = None
-
-    def _validate_access_settings(self):
-        if not all((self.namespace_url, self.queue_name, self.credential)):
-            raise ValueError("Invalid configuration for AzureServiceBus")
-        return None
-
-    @property
-    def client(self):
-        if self._client is None:
-            self._validate_access_settings()
-            self._client = ServiceBusClient(self.namespace_url, self.credential)
-        return self._client
-
-    def get_receiver(self) -> ServiceBusReceiver:
-        if self._receiver_client is not None:
-            return self._receiver_client
-
-        self._receiver_client = self.client.get_queue_receiver(queue_name=self.queue_name)
-        return self._receiver_client
-
-    def get_sender(self) -> ServiceBusSender:
-        if self._sender_client is not None:
-            return self._sender_client
-
-        self._sender_client = self.client.get_queue_sender(queue_name=self.queue_name)
-        return self._sender_client
-
-    async def close(self):
-        if self._receiver_client is not None:
-            await self._receiver_client.close()
-            self._receiver_client = None
-
-        if self._sender_client is not None:
-            await self._sender_client.close()
-            self._sender_client = None
-
-        if self._client is not None:
-            await self._client.close()
-            self._client = None
-
-    async def send_message(self, msg: str, delay: int = 0):
-        message = ServiceBusMessage(msg)
-        now = datetime.datetime.now(tz=datetime.UTC)
-        scheduled_time_utc = now + datetime.timedelta(seconds=delay)
-        sender = self.get_sender()
-        await sender.schedule_messages(message, scheduled_time_utc)
+    def __init__(self, config: Config, credential: DefaultAzureCredential):
+        self.client = ManagedAzureServiceBusSender(
+            config.service_bus_namespace_url,
+            config.service_bus_queue_name,
+            credential,
+            ready_message=STATIC_DEBUG_TASK.model_dump_json(),
+        )
